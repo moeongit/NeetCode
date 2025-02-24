317da075e44ce4174a39e4646266682fd92b9c3c
MoeOnGit <malrobiay04@gmail.com>
2025-02-23 23:45:51 -0700

    Product of Array Except Self: Solved

--- Arrays & Hashing/ProductOfArrayExceptSelf.py
+++ Arrays & Hashing/ProductOfArrayExceptSelf.py
@@ -17,3 +17,22 @@
 # Input: nums = [-1,0,1,2,3]
 
 # Output: [0,-6,0,0,0]
+
+from typing import List
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        result = [1] * len(nums)
+        prefix = 1
+
+        for i in range(len(nums)):
+            result[i] = prefix
+            prefix *= nums[i]
+
+        postfix = 1
+
+        for i in range(len(nums) - 1, -1, -1):
+            result[i] *= postfix
+            postfix *= nums[i]
+
+        return result
