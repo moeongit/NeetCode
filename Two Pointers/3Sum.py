20f9d89ea13b9fbcffc2635c9b3852b2fff02b42
moeongit <malrobiay04@gmail.com>
2025-02-24 18:50:24 -0700

    3Sum

---
+++ Two Pointers/3Sum.py
@@ -0,0 +1,12 @@
+# 3Sum
+# Solved
+
+# Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] where nums[i] + nums[j] + nums[k] == 0, and the indices i, j and k are all distinct.
+
+# The output should not contain any duplicate triplets. You may return the output and the triplets in any order.
+
+# Example 1:
+
+# Input: nums = [-1,0,1,2,-1,-4]
+
+# Output: [[-1,-1,2],[-1,0,1]]
