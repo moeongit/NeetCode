9553ed35182954c612f6e91b4874ca8799c2c02a
moeongit <malrobiay04@gmail.com>
2025-02-26 16:05:18 -0700

    Valid Parantheses: Solved

--- Stack/ValidParantheses.py
+++ Stack/ValidParantheses.py
@@ -28,3 +28,18 @@
 # Input: s = "[(])"
 
 # Output: false
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        mapping = {")":"(", "}":"{", "]":"["}
+        stack = []
+
+        for char in s:
+            if char in mapping.values():
+                stack.append(char)
+            elif char in mapping.keys():
+                if not stack or mapping[char] != stack.pop():
+                    return False
+
+        return not stack
