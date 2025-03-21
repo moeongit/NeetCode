5ff65d5bd36e30865b60389db4f39848a6ec04ad
MoeOnGit <malrobiay04@gmail.com>
2025-03-20 22:47:51 -0600

    VP: Solved

--- Stack/ValidParentheses.py
+++ Stack/ValidParentheses.py
@@ -33,4 +33,13 @@ from typing import List
 
 class Solution:
     def isValid(self, s: str) -> bool:
-        pass
+        stack = []
+        mapping = {"}":"{", "]":"[", ")":"()"}
+
+        for char in s:
+            if char in mapping.values():
+                stack.append(char)
+            elif char in mapping.keys():
+                if not stack or mapping[char] != stack.pop():
+                    return False
+        return not stack
