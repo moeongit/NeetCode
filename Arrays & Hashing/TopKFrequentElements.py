a88b9e9501dd7de1f16042d1536de30c610f0d33
moeongit <malrobiay04@gmail.com>
2025-07-18 21:08:46 -0700

    toml file upgrade

--- Arrays & Hashing/TopKFrequentElements.py
+++ Arrays & Hashing/TopKFrequentElements.py
@@ -22,17 +22,16 @@ from typing import List
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {}
-        freq = [[] for i in range(len(nums) + 1)]
+        freq = {}
 
         for num in nums:
-            count[num] = 1 + count.get(num, 0)
-        for num, cnt in count.items():
-            freq[cnt].append(num)
-        
-        res = []
-        for i in range(len(freq) - 1, 0, -1):
-            for num in freq[i]:
-                res.append(num)
-                if len(res) == k:
-                    return res
+            freq[num] = 1 + freq.get(num, 0)
+
+        heap = []
+
+        for num, count in freq,items():
+            heapq.heappush(heap, (count, num))
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        return [num for (count, num) in heap]
